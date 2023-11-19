7a44b04d7197a44a2caf956c8bb1ba911208e9f0
naitoyuma7110 <naitoyuma1118@gmail.com>
2023-11-20 01:34:21 +0900

    [add] addTodoOnclick action

--- src/components/Todo/TodoForm.tsx
+++ src/components/Todo/TodoForm.tsx
@@ -1,12 +1,20 @@
 import React, { useState } from "react";
 import { Todo } from "./todoItem";
 
-const TodoForm = (): JSX.Element => {
+type TodoFormProps = {
+	addTodoOnclick: (todo: Todo) => void;
+};
+
+const TodoForm = (props: TodoFormProps): JSX.Element => {
 	const [formTodo, setFormTodo] = useState<Todo>({
 		title: "Hello",
 		content: "World",
 		status: "Done",
 	});
+
+	const handlerAddTodoOnclick = () => {
+		props.addTodoOnclick(formTodo);
+	};
 
 	const handlerTodoTitleFormOnChange = (
 		event: React.ChangeEvent<HTMLInputElement>
@@ -16,11 +24,18 @@ const TodoForm = (): JSX.Element => {
 		setFormTodo(newTodo);
 	};
 
-	const handlerAddButtonOnclick = () => {};
+	const handlerTodoContentFormOnChange = (
+		event: React.ChangeEvent<HTMLInputElement>
+	) => {
+		const newTodo = { ...formTodo };
+		newTodo.content = event.target.value;
+		setFormTodo(newTodo);
+	};
 
 	return (
 		<div className="w-100 overflow-hidden bg-white rounded-lg shadow-md dark:bg-gray-800">
 			<p>{formTodo.title}</p>
+			<p>{formTodo.content}</p>
 			<form onSubmit={(e) => e.preventDefault()}>
 				<div className="m-2">
 					<label className="text-gray-400">タイトル</label>
@@ -37,12 +52,13 @@ const TodoForm = (): JSX.Element => {
 					<input
 						type="text"
 						value={formTodo.content}
+						onChange={handlerTodoContentFormOnChange}
 						className="block w-full rounded-md border-0 py-1.5 px-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
 					/>
 				</div>
 				<div className="m-2">
 					<button
-						type="submit"
+						onClick={handlerAddTodoOnclick}
 						className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
 						TODO追加
 					</button>
