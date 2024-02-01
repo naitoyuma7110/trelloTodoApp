98815b53c0cd8edd72760097a0ea50508c5b53d6
naitoyuma7110 <naitoyuma1118@gmail.com>
2024-02-02 00:30:51 +0900

    認証ページのスタイル変更

--- src/pages/auth/login.tsx
+++ src/pages/auth/login.tsx
@@ -1,6 +1,7 @@
 import { getProviders, signIn } from 'next-auth/react'
 import { InferGetServerSidePropsType } from 'next'
 import { FaGithubAlt } from 'react-icons/fa'
+import { Button, Typography } from '@material-tailwind/react'
 
 // getServerSidePropsで取得した値を使用するコンポーネントはSSRによりサーバーサイドで動的に生成される
 const login = ({ providers }: InferGetServerSidePropsType<typeof getServerSideProps>) => {
@@ -8,36 +9,27 @@ const login = ({ providers }: InferGetServerSidePropsType<typeof getServerSidePr
   console.log(providers)
   return (
     <div className='flex flex-col items-center space-y-20 pt-40'>
-      <FaGithubAlt className='w-24 h-24' />
-      <div className='text-center '>
-        <div className='mx-auto max-w-3xl'>
-          <div className='flexjustify-center'>
-            {providers &&
-              Object.values(providers).map((provider) => {
-                return (
-                  <div key={provider.name}>
-                    <button
-                      className='group relative inline-flex items-center justify-start overflow-hidden rounded bg-white px-6 py-3 font-medium transition-all hover:bg-white'
-                      onClick={() =>
-                        // signIn()で"api/auth/{provider}"へ、どのProviderを使用するかid("github","google"など)を指定しリクエスト
-                        // https://next-auth.js.org/configuration/pages
-                        signIn(provider.id, {
-                          // 認証後の遷移先
-                          callbackUrl: '/',
-                        })
-                      }
-                    >
-                      <span className='absolute bottom-0 left-0 mb-9 ml-9 h-48 w-48 -translate-x-full translate-y-full rotate-[-40deg] rounded bg-slate-800 transition-all duration-500 ease-out group-hover:ml-0 group-hover:mb-32 group-hover:translate-x-0'></span>
-                      <span className='relative w-full text-left text-black transition-colors duration-300 ease-in-out group-hover:text-white'>
-                        Sign in with {provider.name}
-                      </span>
-                    </button>
-                  </div>
-                )
-              })}
-          </div>
-        </div>
-      </div>
+      {providers &&
+        Object.values(providers).map((provider) => {
+          return (
+            <div key={provider.name}>
+              <Button
+                className='bg-black flex items-center gap-2'
+                onClick={() =>
+                  // signIn()で"api/auth/{provider}"へ、どのProviderを使用するかid("github","google"など)を指定しリクエスト
+                  // https://next-auth.js.org/configuration/pages
+                  signIn(provider.id, {
+                    // 認証サーバへのリクエスト後の遷移先
+                    callbackUrl: '/',
+                  })
+                }
+              >
+                <FaGithubAlt className='w-10 h-10 text-white' />
+                <Typography>Login with Github</Typography>
+              </Button>
+            </div>
+          )
+        })}
     </div>
   )
 }
