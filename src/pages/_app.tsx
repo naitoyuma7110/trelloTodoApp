a2cff9b7b22efaa9cf0d26dec1a2484b945c10a5
naitoyuma7110 <naitoyuma1118@gmail.com>
2024-02-02 00:31:32 +0900

    パス修正

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,4 +1,4 @@
-import Header from '@/components/Header'
+import Header from '@/components/Header/Header'
 import '@/styles/globals.css'
 import { SessionProvider } from 'next-auth/react'
 import type { AppProps } from 'next/app'
