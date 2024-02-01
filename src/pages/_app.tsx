158e3f29c5b8c796f811ab669c6296ed4e7d1ec1
naitoyuma7110 <naitoyuma1118@gmail.com>
2024-02-01 14:54:42 +0900

    Prismaを介してDBで永続化されたセッション情報をコンポーネントに渡す

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,15 @@
-import "@/styles/globals.css";
-import type { AppProps } from "next/app";
+import Header from '@/components/Header'
+import '@/styles/globals.css'
+import { SessionProvider } from 'next-auth/react'
+import type { AppProps } from 'next/app'
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    // auth/[...nextauth]で設定したoption、adapter: PrismaAdapter(prisma)の部分でセッション情報がDBで永続化されている。
+    // SessionProviderはprismaを介してDBのセッション情報を取得しコンポーネントに渡す事でネストされたコンポーネントで認証情報を参照できる
+    <SessionProvider session={pageProps.session}>
+      <Header />
+      <Component {...pageProps} />;
+    </SessionProvider>
+  )
 }
